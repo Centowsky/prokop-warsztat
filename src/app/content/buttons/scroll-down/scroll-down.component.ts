2326f1f878c60be808fa569ae9d141efcfe22b57
Centowsky <centowsky@gmail.com>
2024-10-04 16:32:02 +0200

    Add function to scrollbutton

--- src/app/content/buttons/scroll-down/scroll-down.component.ts
+++ src/app/content/buttons/scroll-down/scroll-down.component.ts
@@ -1,6 +1,7 @@
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
+import { ScrollService } from '../../../../services/scroll.services';
 
 @Component({
   selector: 'app-scroll-down',
@@ -11,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class ScrollDownComponent {
   faArrowDown = faArrowDown;
+
+  constructor(private scrollService: ScrollService) {}
+
+  scrollToId(id: string) {
+    this.scrollService.scrollToElementById(id);
+  }
 }
